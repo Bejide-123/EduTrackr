f511a7bb34cfd38d2b9402e26fd8529cd9802cb0
Bejide-123 <isbejide10@gmail.com>
2025-07-09 12:59:09 +0100

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { HashRouter } from "react-router-dom";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { HashRouter } from 'react-router-dom';
+import { ClerkProvider } from '@clerk/clerk-react';
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Clerk Publishable Key');
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </ClerkProvider>
   </StrictMode>
-)
+);
+
